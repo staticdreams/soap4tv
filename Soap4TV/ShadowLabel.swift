bef80f1e9234a48d41782d25f14f258416b65085
Peter Tikhomirov <peter.tikhomirov@gmail.com>
2015-12-06 20:10:49 +0100

    Fixes and default login button
    Added common shadow label
    Fixed apis
    fixes in episode views

---
+++ Soap4TV/ShadowLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ShadowLabel.swift
+//  Soap4TV
+//
+//  Created by Peter on 06/12/15.
+//  Copyright © 2015 Peter Tikhomirov. All rights reserved.
+//
+
+import UIKit
+
+class ShadowLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
