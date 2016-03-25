ccb321d15ba8a2b7d63b7b1755c8253600a40f49
Sergey Armodin <mak@arm1.ru>
2016-03-25 19:42:25 +0300

    - Добавлен файл Utils. В нём функция красивого лога с выводом места вызова этой функции, чтобы не искать потом по проекту где обычный print

---
+++ Soap4TV/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  Soap4TV
+//
+//  Created by Sergei Armodin on 25.03.16.
+//  Copyright © 2016 Peter Tikhomirov. All rights reserved.
+//
+
+import Foundation
+
+/**
+Лог-функция с выводом файла, метода и строки, откуда вызывается. Пример использования: DLog("привет")
+
+- parameter message:      текст, который надо вывести
+- parameter fullPath:     путь до файла, который вызывается
+- parameter line:         номер строки в файле
+- parameter functionName: название метода/функции вызова
+*/
+func DLog(message: String, fullPath: String = #file, line: Int = #line, functionName: String = #function) {
+	let file = NSURL.fileURLWithPath(fullPath)
+	print(file.pathComponents!.last);
+	print("\(file.pathComponents!.last) -> \(functionName) -> line \(line) : \(message)")
+}
+
